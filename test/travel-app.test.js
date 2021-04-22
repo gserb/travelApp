67604c3eb0db832b390a6676ae4b248df2151fe6
Anamaria Bodescu <bodescu.anamariia@gmail.com>
2021-04-22 16:13:36 +0300

    Resolve the pre-push test issue

--- test/travel-app.test.js
+++ test/travel-app.test.js
@@ -11,7 +11,7 @@ describe('TravelApp', () => {
   it('renders a h1', () => {
     const h1 = element.shadowRoot.querySelector('h1');
     expect(h1).to.exist;
-    expect(h1.textContent).to.equal('My app');
+    expect(h1.textContent).to.equal('DevSchool app');
   });
 
   it('passes the a11y audit', async () => {
