dcf58ef2116a35f1e4e8ee58eb956c4b8a05341e
Anamaria Bodescu <bodescu.anamariia@gmail.com>
2021-05-04 04:24:51 +0300

    Add media queries

---
+++ Session/main.js
@@ -0,0 +1,16 @@
+var x=1;
+function (name){
+    console.log(nume+' rocks'+lastName);
+}
+
+var printName = function(nume){
+    console.log(nume+' rocks');
+}
+
+var lastName = 1;
+var sumNumbers = function(firstNumber){
+    return (secondNumber) =>{ return firstNumber + secondNumber};
+}
+
+sumNumbers(1)(2)
+
