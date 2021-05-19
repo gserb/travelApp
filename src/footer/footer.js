419fed6a00f907ddbc41411ee4d282715b3531fd
Vlad4496 <vlad_4496@yahoo.com>
2021-05-19 20:18:58 +0300

    refactor: changed the navbar and footer into web components

---
+++ src/footer/footer.js
@@ -0,0 +1,183 @@
+import { LitElement, html, css } from 'lit-element';
+
+class Footer extends LitElement {
+  static get styles() {
+    return css`
+      footer {
+        /* background-color: rgba(0, 82, 254, 255); */
+        /* background-color: rgb(38, 38, 204); */
+        background-color: #8392da;
+        color: white;
+        width: 100vw;
+      }
+
+      footer .getInTouch {
+        text-align: center;
+        margin-bottom: 5em;
+      }
+      footer .getInTouch h1 {
+        margin-bottom: 0.5em;
+        padding-top: 1em;
+        font-size: 3em;
+        margin-top: 0;
+      }
+      footer .getInTouch p {
+        font-size: 1.2em;
+        margin-bottom: 1.7em;
+      }
+      footer .getInTouch input[type='email'] {
+        padding: 3em 0em;
+        width: 40%;
+        border-radius: 4em;
+        position: relative;
+        border: none;
+      }
+      footer .getInTouch input::placeholder {
+        font-size: 1.2em;
+        font-weight: bold;
+        width: 100vw;
+        padding-left: 3em;
+        position: fixed;
+        color: #8d98bd;
+      }
+      footer .getInTouch input[type='button'] {
+        padding: 1.7em 3em;
+        border-radius: 3em;
+        /* background-color: rgba(0, 82, 254, 255); */
+        /* background-color: rgb(38, 38, 204); */
+        background-color: #6f85ce;
+        color: white;
+        font-weight: bold;
+        font-size: 1.2em;
+        position: absolute;
+        right: 30%;
+        margin-top: 0.2em;
+        border: none;
+        cursor: pointer;
+      }
+      footer .getInTouch input[type='button']:hover {
+        background-color: rgba(60, 60, 255, 0.2);
+        color: white;
+        transition: 1s;
+      }
+
+      footer .contacts {
+        border-top: 1px solid white;
+        padding-top: 2em;
+        padding-bottom: 2em;
+        display: flex;
+        width: 100vw;
+        justify-content: space-around;
+      }
+      footer .contacts ul {
+        display: flex;
+      }
+      footer .contacts ul li {
+        list-style-type: none;
+        margin-right: 2em;
+        font-weight: bold;
+        cursor: pointer;
+      }
+      footer .contacts ul li:hover {
+        color: rgb(38, 38, 204);
+        transform: scale(1.5);
+        transition: 0.5s;
+      }
+      @media screen and (min-width: 1025px) and (max-width: 1200px) {
+        footer .getInTouch input[type='email'] {
+          width: 60vw;
+        }
+      }
+      @media screen and (min-width: 769px) and (max-width: 1024px) {
+        footer .getInTouch input[type='button'] {
+          position: relative;
+          left: 5%;
+          background-color: red;
+        }
+      }
+      @media screen and (min-width: 481px) and (max-width: 768px) {
+        footer .getInTouch {
+          font-size: 0.7em;
+          justify-content: center;
+        }
+        footer .getInTouch input[type='email'] {
+          display: block;
+          width: 60vw;
+          font-size: 1em;
+        }
+        footer .getInTouch input[type='button'] {
+          position: relative;
+          left: 20%;
+          background-color: red;
+        }
+        footer .contacts {
+          flex-wrap: wrap;
+        }
+      }
+      @media screen and (min-width: 320px) and (max-width: 480px) {
+        footer .getInTouch {
+          font-size: 0.7em;
+          justify-content: center;
+        }
+        footer .getInTouch input[type='email'] {
+          display: block;
+          width: 60vw;
+          font-size: 0.7em;
+        }
+        footer .getInTouch input[type='button'] {
+          position: relative;
+          left: 20%;
+          background-color: red;
+        }
+        footer .contacts {
+          flex-wrap: wrap;
+          font-size: 0.7em;
+        }
+      }
+    `;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+  }
+
+  static get properties() {
+    return {
+      _isLoading: {
+        type: Boolean,
+      },
+    };
+  }
+
+  render() {
+    if (this._isLoading) {
+      return 'Loading...';
+    }
+    return html` <footer id="contacts">
+      <div class="getInTouch">
+        <h1>Get in touch</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, quae!
+        </p>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Enter your email address"
+        />
+        <input type="button" value="Subscribe" />
+      </div>
+      <div class="contacts">
+        <h3>&#169; 2020 india cafe all rights reserved</h3>
+        <ul>
+          <li>Home</li>
+          <li>Tour</li>
+          <li>Hotel</li>
+          <li>Places</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+    </footer>`;
+  }
+}
+customElements.define('footer-component', Footer);
---
+++ src/navBar/navBar.js
@@ -0,0 +1,190 @@
+import { LitElement, html, css } from 'lit-element';
+
+class NavBar extends LitElement {
+  static get styles() {
+    return css`
+      nav {
+        position: absolute;
+        top: 10%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        display: flex;
+        width: 100vw;
+        justify-content: space-around;
+      }
+      nav h1 {
+        color: white;
+        font-size: 3em;
+      }
+      nav ul {
+        display: flex;
+        align-items: center;
+      }
+      nav ul li {
+        list-style-type: none;
+        margin-right: 1em;
+        font-size: 1.5em;
+        font-weight: 600;
+      }
+      nav ul li:hover {
+        transform: scale(1.3);
+        transition: 0.5s;
+      }
+      nav ul li a:hover {
+        color: pink;
+      }
+      nav ul li a {
+        text-decoration: none;
+        color: black;
+      }
+      nav ul input {
+        padding: 1em 2em;
+        border-radius: 1em;
+        font-size: 1.3em;
+        border-style: none;
+        background-color: #5b6bb1;
+        color: white;
+        font-weight: 600;
+        cursor: pointer;
+      }
+      nav ul input:hover {
+        background-color: #d0d3df;
+        color: #5b6bb1;
+        transition: 1s;
+      }
+      .hero {
+        position: absolute;
+        top: 35%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 40vw;
+      }
+      .hero h1 {
+        font-size: 5em;
+        text-align: center;
+        color: white;
+      }
+      .hero input {
+        padding: 1.7em 3.5em;
+        border-radius: 1em;
+        border-style: none;
+        color: white;
+        background-color: #5b6bb1;
+        font-size: large;
+        position: relative;
+        left: 50%;
+        transform: translate(-50%);
+        margin-top: 2em;
+        cursor: pointer;
+      }
+
+      .hero input:hover {
+        background-color: white;
+        color: #5b6bb1;
+        transition: 1s;
+      }
+      .parallax {
+        background-image: url('https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80');
+        background-size: cover;
+        background-attachment: fixed;
+        background-position: center;
+        height: 80vh;
+        width: 100vw;
+      }
+      @media screen and (min-width: 1201px) {
+        nav {
+          width: 80%;
+        }
+      }
+      @media screen and (min-width: 1025px) and (max-width: 1200px) {
+        nav {
+          max-width: 900px;
+        }
+        .hero h1 {
+          font-size: 3.5em;
+        }
+      }
+      @media screen and (min-width: 769px) and (max-width: 1024px) {
+        nav {
+          font-size: 0.7em;
+        }
+        .hero h1 {
+          font-size: 3em;
+        }
+      }
+      @media screen and (min-width: 481px) and (max-width: 768px) {
+        nav {
+          text-align: center;
+          margin-top: 5em;
+        }
+        nav ul {
+          font-size: 1em;
+          flex-direction: column;
+        }
+        nav ul li {
+          margin-left: 1em;
+          line-height: 1.5em;
+          display: inline-block;
+        }
+        .hero h1 {
+          font-size: 1.8em;
+          margin-top: 10em;
+        }
+      }
+      @media screen and (min-width: 320px) and (max-width: 480px) {
+        nav {
+          flex-wrap: wrap;
+          flex-direction: column;
+          margin-top: 5em;
+          text-align: center;
+        }
+        nav ul {
+          flex-direction: column;
+        }
+        nav ul li {
+          margin-left: 1em;
+          line-height: 1.5em;
+        }
+        .hero h1 {
+          font-size: 2em;
+          margin-top: 12em;
+        }
+      }
+    `;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+  }
+
+  static get properties() {
+    return {
+      _isLoading: { type: Boolean },
+    };
+  }
+
+  render() {
+    if (this._isLoading) {
+      return 'Loading...';
+    }
+    return html` <header>
+      <div class="parallax"></div>
+      <nav>
+        <h1>Travela</h1>
+        <ul>
+          <li><a href="./index.html">Home</a></li>
+          <li><a href="#">Tour</a></li>
+          <li><a href="#">Hotel</a></li>
+          <li><a href="#placesToTravel">Places to travel</a></li>
+          <li><a href="#contacts">Contact Us</a></li>
+          <input type="button" value="Book now" />
+        </ul>
+      </nav>
+      <div class="hero">
+        <h1>Discover the world with us</h1>
+        <input type="button" value="Start Tour" />
+      </div>
+    </header>`;
+  }
+}
+customElements.define('navbar-component', NavBar);
