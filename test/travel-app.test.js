dc76f29a20448938e051edd4d9bd1fbd056efaa2
Vlad4496 <vlad_4496@yahoo.com>
2021-06-02 17:00:10 +0300

    feat: wip  unit test

--- test/travel-app.test.js
+++ test/travel-app.test.js
@@ -1,20 +1,33 @@
-import { html, fixture, expect } from '@open-wc/testing';
+const playwright = require('playwright');
 
-import '../src/travel-app.js';
-
-describe('TravelApp', () => {
-  let element;
-  beforeEach(async () => {
-    element = await fixture(html`<travel-app></travel-app>`);
+(async () => {
+  const browserType = ['chromium'];
+  const browser = await playwright[browserType].launch({
+    headless: false,
   });
 
-  // it('renders a h1', () => {
-  //   const h1 = element.shadowRoot.querySelector('h1');
-  //   expect(h1).to.exist;
-  //   expect(h1.textContent).to.equal('My app');
-  // });
+  const context = await browser.newContext();
 
-  it('passes the a11y audit', async () => {
-    await expect(element).shadowDom.to.be.accessible();
+  const page = await context.newPage();
+
+  await page.screenshot({
+    path: `screenshot-${browserType}.png`,
   });
-});
+
+  await page.goto('https://travelatravelapp.netlify.app/');
+
+  const [request] = await Promise.all([
+    page.waitForEvent('requestfinished'),
+    page.goto('https://travelatravelapp.netlify.app/places-to-travel'),
+  ]);
+  console.log(request.timing());
+
+  const fontSize = await page.$eval(
+    'div',
+    el => window.getComputedStyle(el).fontSize
+  );
+  expect(fontSize === '16px').toBeTruthy();
+  console.log(fontSize);
+
+  await browser.close();
+})();
